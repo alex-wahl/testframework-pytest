add25d677d4b26341007bc76d920ad495035ae52
Alex <alexander.wahl@zattoo.com>
2024-03-02 19:57:02 +0100

    screenshots and isort

--- tests/web/ikea/pom/base_page.py
+++ tests/web/ikea/pom/base_page.py
@@ -1,7 +1,8 @@
-from libs.helper import SingletonMeta
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+from libs.helper import SingletonMeta
 
 
 class BasePage(metaclass=SingletonMeta):
