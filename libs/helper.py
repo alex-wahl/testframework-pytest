ae9133b6670d9a567e7bbbe7d4177e88c0913d59
Alex <alexander.wahl@zattoo.com>
2024-03-02 17:00:18 +0100

    update create_driver

--- libs/helper.py
+++ libs/helper.py
@@ -1,12 +1,15 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.chrome import ChromeDriverManager
 
 
-def create_driver(browser, driver_path, options):
+def create_driver(browser: str, options: webdriver.ChromeOptions or webdriver.FirefoxOptions):
     if browser == "firefox":
-        return webdriver.Firefox(service=Service(driver_path), options=options)
+        return webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()), options=options)
     elif browser == "chrome":
-        return webdriver.Chrome(service=Service(driver_path), options=options)
+        return webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
 
 
 class SingletonMeta(type):
