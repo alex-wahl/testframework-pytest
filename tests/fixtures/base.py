e954a8eac628a370244fd09c7ec473d06eb9f915
Alex <alexander.wahl@zattoo.com>
2024-03-02 17:00:55 +0100

    small improvments

--- tests/fixtures/base.py
+++ tests/fixtures/base.py
@@ -3,25 +3,22 @@ import pytest
 from libs.helper import create_driver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
-from libs.constants import WEBDRIVER
 
 
 @pytest.fixture()
 def setup():
     os_browser_mapping = {
-        "linux": ("firefox", "geckodriver_linux", FirefoxOptions),
-        "darwin": ("chrome", "chromedriver_mac", ChromeOptions),
+        "linux": ("firefox", FirefoxOptions),
+        "darwin": ("chrome", ChromeOptions),
     }
 
     operating_system = next((os for os in os_browser_mapping if sys.platform.startswith(os)), None)
     if operating_system:
-        browser, driver_name, OptionsClass = os_browser_mapping[operating_system]
+        browser, OptionsClass = os_browser_mapping[operating_system]
         options = OptionsClass()
         options.add_argument("--start-maximized")
         options.add_argument("--headless")
-        driver_path = f"{WEBDRIVER}/{driver_name}"
-        print(driver_path)
-        driver = create_driver(browser, driver_path, options)
+        driver = create_driver(browser, options)
         yield driver
         driver.quit()
     else:
