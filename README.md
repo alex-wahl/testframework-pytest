224deef37649129ec4ad1050264b51e99ac0443c
Alex <alexander.wahl@zattoo.com>
2024-03-03 20:16:55 +0100

    Search an item

--- README.md
+++ README.md
@@ -3,6 +3,14 @@
 # Test framework based on pytest
 Test framework to test backend, frontend and mobile apps
 
+## Dependencies:
+- Python 3.11 and above
+- Docker
+- Allure (install [allure command line tool](https://allurereport.org/docs/gettingstarted-installation/))
+- Pytest
+- Selenium
+- Poetry
+
 ## Patterns:    
 - Page Object Model
 
@@ -13,4 +21,9 @@ docker compose run --rm testframework -s tests/web/ikea/test_ikea.py --env PROD
 pytest -s tests/web/ikea/test_ikea.py --env PROD
 
 pytest -m cookies -s tests/web/ikea/test_ikea.py --env PROD
+```
+
+## Check Allure reports:
+```bash
+allure serve allure-results
 ```
--- pom/ikea/home_page.py
+++ pom/ikea/home_page.py
@@ -4,7 +4,19 @@ from selenium.webdriver.common.by import By
 
 class HomePage(BasePage):
     COOCKIES_BANNER_ACCEPT_BUTTON = "onetrust-accept-btn-handler"
+    SEARCH_ID_FIELD = "ikea-search-input"
+    SEARCH_SUMMARY_CLASS = "search-summary__heading b"
 
     def accept_coockies(self):
         self.wait_until_element_is_visible(By.ID, self.COOCKIES_BANNER_ACCEPT_BUTTON)
         self.click_element(By.ID, self.COOCKIES_BANNER_ACCEPT_BUTTON)
+
+    def search_for(self, search_phrase: str):
+        self.accept_coockies()
+        self.wait_until_element_is_visible(By.ID, self.SEARCH_ID_FIELD)
+        self.driver.find_element(By.ID, self.SEARCH_ID_FIELD).send_keys(search_phrase)
+        self.driver.find_element(By.ID, self.SEARCH_ID_FIELD).submit()
+
+    def get_search_summary(self):
+        self.wait_until_element_is_visible(By.CLASS_NAME, self.SEARCH_SUMMARY_CLASS)
+        return self.driver.find_element(By.CLASS_NAME, self.SEARCH_SUMMARY_CLASS).text
